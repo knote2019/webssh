a1de2c2cfa457bca818f006447f6ddeb291fce49
KennyKang <227105027+KennyKangNV2025@users.noreply.github.com>
2025-11-06 16:23:09 +0800

    fix

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     packages=['webssh'],
     entry_points='''
     [console_scripts]
-    wssh = webssh.main:main
+    webssh = webssh.main:main
     ''',
     license='MIT',
     include_package_data=True,
