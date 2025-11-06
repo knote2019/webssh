8cf83f05fe5e1395e945f13c55ae19a2e6a07c7b
KennyKang <227105027+KennyKangNV2025@users.noreply.github.com>
2025-11-06 16:04:35 +0800

    fix

---
+++ README.md
@@ -0,0 +1,18 @@
+Web-SSH
+------
+
+workflow.
+::
+
+    +---------+     http     +-----------------------+
+    | browser | <==========> | webssh <=>  ssh server|
+    +---------+   websocket  +-----------------------+
+
+install:
+::
+    python setup.py install
+    wssh --port=6622 --ssh-port=22 --ssh-username=<SSH-USERNAME> --ssh-password=<SSH-PASSWORD>
+
+login:
+::
+    http://10.176.196.66:6622
--- setup.py
+++ setup.py
@@ -1,36 +1,19 @@
-import codecs
 from setuptools import setup
 from webssh._version import __version__ as version
-
-
-with codecs.open('README.rst', encoding='utf-8') as f:
-    long_description = f.read()
 
 
 setup(
     name='webssh',
     version=version,
-    description='Web based ssh client',
-    long_description=long_description,
-    author='Shengdun Hua',
-    author_email='webmaster0115@gmail.com',
-    url='https://github.com/huashengdun/webssh',
+    description='webssh',
+    author='kenny',
+    url='https://github.com/knote2019/webssh',
     packages=['webssh'],
     entry_points='''
     [console_scripts]
-    wssh = webssh.main:main
+    webssh = webssh.main:main
     ''',
     license='MIT',
     include_package_data=True,
-    classifiers=[
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
-    ],
-    install_requires=[
-        'tornado>=4.5.0',
-        'paramiko>=2.3.1',
-    ],
+    install_requires=['paramiko>=2.3.1', 'tornado>=4.5.0'],
 )
