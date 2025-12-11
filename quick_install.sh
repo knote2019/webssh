8293a5fc181cdb428ad81dbcb57767321d7c91dc
KennyKang <227105027+KennyKangNV2025@users.noreply.github.com>
2025-12-11 12:42:32 +0800

    fix arm timeout issue

--- quick_install.sh
+++ quick_install.sh
@@ -4,6 +4,9 @@ SSH_USERNAME=${SSH_USERNAME:-root}
 SSH_PASSWORD=${SSH_PASSWORD:-cloud1234}
 SSH_PORT=${SSH_PORT:-5522}
 WEB_SSH_PORT=${WEB_SSH_PORT:-6622}
+SSH_TIMEOUT=${SSH_TIMEOUT:-30}
+WS_PING_INTERVAL=${WS_PING_INTERVAL:-30}
+WORKER_DELAY=${WORKER_DELAY:-10}
 
 echo "========================================================================="
 echo "WebSSH:"
@@ -13,8 +16,8 @@ apt-get install -y openssh-server > /dev/null 2>&1 || exit 1
 sed -i "s/#Port.*/Port $SSH_PORT/" /etc/ssh/sshd_config
 sed -i 's/#PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
 sed -i 's/#PasswordAuthentication.*/PasswordAuthentication yes/' /etc/ssh/sshd_config
-sed -i 's/#ClientAliveInterval.*/ClientAliveInterval 0/' /etc/ssh/sshd_config
-sed -i 's/#ClientAliveCountMax.*/ClientAliveCountMax 0/' /etc/ssh/sshd_config
+sed -i 's/#ClientAliveInterval.*/ClientAliveInterval 60/' /etc/ssh/sshd_config
+sed -i 's/#ClientAliveCountMax.*/ClientAliveCountMax 3/' /etc/ssh/sshd_config
 echo "$SSH_USERNAME:$SSH_PASSWORD" | chpasswd
 
 mkdir -p /run/sshd
@@ -22,7 +25,7 @@ mkdir -p /run/sshd
 echo "SSH started (Port: $SSH_PORT)"
 
 python setup.py install > /dev/null 2>&1 || exit 1
-webssh --port="$WEB_SSH_PORT" --ssh-port="$SSH_PORT" --ssh-username="$SSH_USERNAME" --ssh-password="$SSH_PASSWORD" &
+webssh --port="$WEB_SSH_PORT" --ssh-port="$SSH_PORT" --ssh-username="$SSH_USERNAME" --ssh-password="$SSH_PASSWORD" --timeout="$SSH_TIMEOUT" --wpintvl="$WS_PING_INTERVAL" --delay="$WORKER_DELAY" &
 echo "WebSSH started (Port: $WEB_SSH_PORT)"
 
 echo "========================================================================="
--- webssh/settings.py
+++ webssh/settings.py
@@ -42,9 +42,9 @@ define('origin', default='same', help='''Origin policy,
 '<domains>': custom domains policy, matches any domain in the <domains> list
 separated by comma;
 '*': wildcard policy, matches any domain, allowed in debug mode only.''')
-define('wpintvl', type=float, default=0, help='Websocket ping interval')
-define('timeout', type=float, default=3, help='SSH connection timeout')
-define('delay', type=float, default=3, help='The delay to call recycle_worker')
+define('wpintvl', type=float, default=30, help='Websocket ping interval')
+define('timeout', type=float, default=30, help='SSH connection timeout')
+define('delay', type=float, default=10, help='The delay to call recycle_worker')
 define('maxconn', type=int, default=20,
        help='Maximum live connections (ssh sessions) per client')
 define('ssh_host', default='localhost', help='Default SSH host')
